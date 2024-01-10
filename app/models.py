715fae750ddc15fa7158d277a4ba671c1dcfa7fc
Dan Leonardson <oscarpoppa@protonmail.com>
2024-01-10 17:31:05 -0600

    switched to mysql & changed hash length in db

--- app/models.py
+++ app/models.py
@@ -11,7 +11,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    password_hash = db.Column(db.String(256))
 
     def set_password(self, pswd):
         self.password_hash = generate_password_hash(pswd)
