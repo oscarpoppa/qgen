2ba2448cb7e5d08655463b90fc8e18fe523c64c5
oscarpoppa <69337264+oscarpoppa@users.noreply.github.com>
2024-03-05 10:00:19 -0600

    Update config.py
    removed specifics

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://dan:Mabrook2@localhost/quiz'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://user:password/db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'whatever...'
 
