85735b4005037b6cff6c28e39697d9ce2f98e622
Dan Leonardson <oscarpoppa@protonmail.com>
2024-03-17 18:32:12 -0500

    added flag

--- app/models.py
+++ app/models.py
@@ -13,6 +13,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(256))
     is_admin = db.Column(db.Boolean, default=False)
+    pw_man_reset = db.Column(db.Boolean, default=False)
 
     def set_password(self, pswd):
         self.password_hash = generate_password_hash(pswd)
