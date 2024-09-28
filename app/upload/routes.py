6934cc7ccb648c7b404184a84e676929fdfb0919
oscarpoppa <oscarpoppa@protonmail.com>
2024-09-28 17:59:09 -0500

    added comments

--- app/upload/routes.py
+++ app/upload/routes.py
@@ -10,6 +10,7 @@ from os import listdir, remove
 
 STATIC = '/home/dan/proj/quiz/app/static/'
 
+#try to create a thumbnail
 def trythumb(path, fname):
     fpath = path + fname
     try:
@@ -22,6 +23,7 @@ def trythumb(path, fname):
     except Exception as exc:
         pass
 
+#admin-only upload image or file to server
 @upload_bp.route('/upload', methods=['POST','GET'])
 @login_required
 @pw_check
@@ -40,6 +42,7 @@ def upload():
         return redirect(url_for('upload.upload'))
     return render_template('upload.html', title='Upload a File', form=form)
 
+#admin-only list images on server
 @upload_bp.route('/images', methods=['GET'])
 @login_required
 @pw_check
@@ -48,6 +51,7 @@ def images():
     imgs = [(f,f[2:]) for f in listdir(STATIC) if f.startswith('T_')]
     return render_template('images.html', imgs=imgs, title='Images')
 
+#admin-only list non-image files on server
 @upload_bp.route('/nonimages', methods=['GET'])
 @login_required
 @pw_check
@@ -60,6 +64,7 @@ def nonimages():
     nonims = [f for f in allf if f not in imgs]
     return render_template('nonimages.html', files=nonims, title='Non-Image Files')
 
+#admin-only delete an image from server
 @upload_bp.route('/delimg/<fname>', methods=['GET'])
 @login_required
 @pw_check
@@ -77,6 +82,7 @@ def delimg(fname):
             flash('Deletion failed for {}'.format(fname))
     return redirect(url_for('upload.images'))
 
+#admin-only delete a non-image from server
 @upload_bp.route('/delnonimg/<fname>', methods=['GET'])
 @login_required
 @pw_check
