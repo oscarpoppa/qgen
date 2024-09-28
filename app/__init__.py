91af60370e9d285bc2159bffd1bdd055e42d1b38
oscarpoppa <69337264+oscarpoppa@users.noreply.github.com>
2024-09-28 17:11:48 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+#build app
 app = Flask(__name__)
 app.config.from_object(Config)
 app.logger.setLevel(3)
@@ -12,6 +13,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'user.login'
 
+#gather and register blueprints
 from app.error import error_bp
 from app.apiv1 import api_bp
 from app.qgen import qgen_bp
@@ -24,6 +26,7 @@ app.register_blueprint(qgen_bp)
 app.register_blueprint(user_bp)
 app.register_blueprint(upload_bp)
 
+#create CLI command for DB dump
 from app.commands import dbdump as dbdump_cli_group
 app.cli.add_command(dbdump_cli_group, name='dbdump')
 
