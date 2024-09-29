ff8eea6501101742f9b48af4a4e0a9e37d5b0cc2
Dan Leonardson <69337264+oscarpoppa@users.noreply.github.com>
2024-09-29 14:07:42 -0500

    Update handlers.py

--- app/error/handlers.py
+++ app/error/handlers.py
@@ -1,11 +1,13 @@
 from . import db, error_bp
 from flask import render_template, flash
 
+#handler for 404 error
 @error_bp.app_errorhandler(404)
 def notfound(error):
     flash(error)
     return render_template('404.html'), 404
 
+#handler for 500 error
 @error_bp.app_errorhandler(500)
 def interr(error):
     flash(error)
