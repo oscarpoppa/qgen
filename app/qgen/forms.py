c03b303cf3987b6a0a6ba5366ee4aaa9ce62c9eb
Dan Leonardson <69337264+oscarpoppa@users.noreply.github.com>
2024-09-29 14:24:58 -0500

    Update forms.py

--- app/qgen/forms.py
+++ app/qgen/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
 from app.user.models import User
 
+#form for virtual problem creation page
 class VProbAdd(FlaskForm):
     rawprob = StringField('Raw Problem', validators=[DataRequired()])
     rawansr = StringField('Raw Answer', validators=[DataRequired()])
@@ -13,7 +14,7 @@ class VProbAdd(FlaskForm):
     calculator_ok = BooleanField('Calculator OK')
     submit = SubmitField('Submit')
 
-
+#form for virtual quiz creation page
 class VQuizAdd(FlaskForm):
     vplist = StringField('VProblem List', validators=[DataRequired()])
     title = StringField('Quiz Title', validators=[DataRequired()])
